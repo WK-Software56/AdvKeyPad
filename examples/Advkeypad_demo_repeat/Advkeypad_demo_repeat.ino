8a1d510873f80a43b7d221a9b824957fd0af4bcf
Walter Kvapil <127616298+WK-Software56@users.noreply.github.com>
2023-03-21 18:26:32 +0100

    Add files via upload

---
+++ examples/Advkeypad_demo_repeat/Advkeypad_demo_repeat.ino
@@ -0,0 +1,54 @@
+//
+//    FILE: Advkeypad_demo.ino
+//  AUTHOR: Walter Kvapil
+// PURPOSE: demo
+//     URL: https://github.com/WK-Software56/AdvKeyPad
+//
+//  PCF8574
+//    pin p0-p3 rows
+//    pin p4-p7 columns
+//  4x4 or smaller keypad.
+
+
+#include "Wire.h"
+#include "AdvKeyPad.h"
+
+const uint8_t KEYPAD_ADDRESS = 0x20;
+
+AdvKeyPad keyPad(KEYPAD_ADDRESS);
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+
+  Wire.begin();
+  Wire.setClock(400000);
+  if (keyPad.begin() == false)
+  {
+    Serial.println("\nERROR: cannot communicate to keypad.\nPlease reboot.\n");
+    while(1);
+  }
+  // Using standard CharMap for 4X4 Keypad
+  // No autorepeat because Delay not set
+  // To enable Autorepeat uncomment the neyt line
+  keyPad.setRepeat(1000,300);
+  
+}
+
+
+void loop()
+{
+  uint8_t key;
+  
+  key=keyPad.getKey();
+  if(key != KP__NOKEY) {
+    Serial.print(key);
+    Serial.print("\t");
+    Serial.println((char)keyPad.getLastChar());
+  }
+}
+
+
+// -- END OF FILE --
